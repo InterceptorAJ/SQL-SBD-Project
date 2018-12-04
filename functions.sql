f7744dbd3f3b4413ae40f7c397424443e4e4528d
AJ <44204536+InterceptorAJ@users.noreply.github.com>
2018-12-04 22:31:32 +0100

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -12,3 +12,24 @@ END;
 $$	LANGUAGE plpgsql;
 
 SELECT importxml('2018-03-01.xml') -- file should be inside "\PostgreSQL\10\data" folder
+
+
+-- function that export all rates and dates from xmltree to rates table
+CREATE OR REPLACE FUNCTION exportrates() RETURNS void AS $$
+
+BEGIN
+INSERT INTO rates 
+(rate, edate)
+SELECT
+	(xpath('//ExchangeRatesTable/Rates/Rate', xml_element))::text::xml,
+	unnest(xpath('//ExchangeRatesTable/EffectiveDate/text()', xml_element::xml))::text::date
+FROM (
+	SELECT unnest(xpath('//ArrayOfExchangeRatesTable',xmltree)) AS xml_element FROM cache
+) t;
+END;
+$$ LANGUAGE 'plpgsql';	
+
+--execution of this function
+SELECT exportrates()
+                
+                
